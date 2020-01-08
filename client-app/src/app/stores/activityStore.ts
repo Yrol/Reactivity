67df3b6627f421c5ddda32a6bd9ee368df0a1570
unknown <yrollgayanth@yahoo.com>
2020-01-08 23:13:29 +1100

    Integrating MobX with selectedActivity: Phase 1

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,7 +5,9 @@ import { createContext } from 'react';
 
 class ActivityStore {
     @observable activities: IActivity[] = [];
-    @observable loadingInitial = false
+    @observable selectedActivity : IActivity | null = null;
+    @observable loadingInitial = false;
+    @observable editMode = false;
 
     @action loadActivities = () => {
         this.loadingInitial = true;
@@ -13,10 +15,14 @@ class ActivityStore {
             response.forEach((activity) => {
               //loop through the API response.data
               activity.date = activity.date.split(".")[0]; //splitting the time before the dot(.)
-              console.log(activity);
               this.activities.push(activity);
             });
           }).then(() => this.loadingInitial = false);
+    }
+
+    @action selectActivity = (id: string) => {
+        this.selectedActivity = this.activities.filter(a => a.id === id)[0];
+        this.editMode = false;
     }
 }
 
