40d27530e54ee05dd460de5b0ef4ba00e6e5544f
unknown <yrollgayanth@yahoo.com>
2020-01-14 21:41:20 +1100

    Retain scroll when forward or back buttons pushed

--- client-app/src/app/layouts/ScrollToTop.tsx
+++ client-app/src/app/layouts/ScrollToTop.tsx
@@ -1,11 +1,14 @@
 /** Implementation to scroll up always when go back or go to a new page */
 /** Used in "index.tsx" */
-import {withRouter} from 'react-router-dom'
+import {withRouter, RouteComponentProps} from 'react-router-dom'
 import {useEffect} from 'react'
 
-const ScrollToTop = ({children, match, location: {pathname}} : any) => {
+const ScrollToTop: React.FC<RouteComponentProps> = ({children, match, history, location: {pathname}} : any) => {
     useEffect(() => {
-        window.scrollTo(0, 0)
+        //PUSH - if the component is re-rendered only. Hence, back button and forward will retain the previous scroll positions since they're "POP"
+        if (history.action === 'PUSH'){
+            window.scrollTo(0, 0)
+        }
     }, [pathname])
 
     return children
