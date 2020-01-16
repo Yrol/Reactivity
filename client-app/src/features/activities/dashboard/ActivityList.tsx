01be2d5a33e53febe897f34ff5592a0991968cbf
unknown <yrollgayanth@yahoo.com>
2020-01-16 22:05:02 +1100

    Adding styles to the items group

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,51 +2,52 @@ import React, { Fragment, SyntheticEvent, useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
 import { IActivity } from "../../../models/activity";
 import { observer } from "mobx-react-lite";
-import ActivityStore from '../../../app/stores/activityStore'
+import ActivityStore from "../../../app/stores/activityStore";
 import { Link } from "react-router-dom";
-import ActivityListItem  from "./ActivityListItem"
+import ActivityListItem from "./ActivityListItem";
 
 interface IProps {
   //activities: IActivity[];
-
   //referencing the function "currentSelectedActivity" defined in App.tsx to get the selected ID
   //currentSelectedActivity: (id: string) => void;
-
   //setEditMode: (emode: boolean) => void;
-
   //handler for deleting an activity. Accepts event of SyntheticEvent type which contains button properties such as the unique button name (unique ID)
   //deleteActivity: (e:SyntheticEvent<HTMLButtonElement> ,id: string) => void;
-
   //submitState:boolean;
-
   //deleteActivityID:string;//contains the unique ID of the clicked button
 }
 /** Setting IProps and destructuring them - such as activities, currentSelectedActivity and etc...  */
-const ActivityList: React.FC<IProps> = ({
-  //activities,
-  //currentSelectedActivity,
-  //setEditMode,
-  //deleteActivity,
-  //submitState,
-  //deleteActivityID
-}) => {
+const ActivityList: React.FC<IProps> = (
+  {
+    //activities,
+    //currentSelectedActivity,
+    //setEditMode,
+    //deleteActivity,
+    //submitState,
+    //deleteActivityID
+  }
+) => {
   //Defining the MobX store (ActivityStore) and destructuring the required functions and variables from it
   const activityStore = useContext(ActivityStore);
-  const {activitiesByDate, setSelectActivity, submitState, deleteActivity, deleteActivityId} = activityStore
+  const {
+    activitiesByDate,
+    setSelectActivity,
+    submitState,
+    deleteActivity,
+    deleteActivityId
+  } = activityStore;
   return (
     <Fragment>
       {activitiesByDate.map(([group, activities]) => (
         <Fragment key={group}>
-          <Label size='large' color='blue'>
+          <Label size="large" color="blue">
             {group}
-        </Label>   
-        <Segment clearing>
+          </Label>
             <Item.Group divided>
               {activities.map(activity => (
-                <ActivityListItem key={activity.id} activity={activity}/>
+                <ActivityListItem key={activity.id} activity={activity} />
               ))}
             </Item.Group>
-          </Segment>       
         </Fragment>
       ))}
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,53 +1,63 @@
-import React, { useContext } from 'react';
-import { Item, Button, Label } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import { IActivity } from '../../../models/activity';
-import ActivityList from './ActivityList';
-import { observer } from 'mobx-react-lite';
+import React, { useContext } from "react";
+import { Item, Button, Label, Segment, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { IActivity } from "../../../models/activity";
+import ActivityList from "./ActivityList";
+import { observer } from "mobx-react-lite";
 
-const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
-    return (
-        <Item>
+const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="/assets/logo192.png" />
             <Item.Content>
               <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.description}</div>
-                <div>{activity.city}</div>
-              </Item.Description>
+              <Item.Description>Hosted by Bob</Item.Description>
               <Item.Extra>
-                {/** currentSelectedActivity is the handleSelectedActivity handler passed/originated in App.tsx */}
-                {/** setEditMode is the handleSelectedActivity handler passed/originated in App.tsx */}
-                <Button
-                  as={Link} to={`/activities/${activity.id}`} // Routes integration
-                  // onClick={() => {
-                  //   setSelectActivity(activity.id) //After MobX integration
-                  //   //currentSelectedActivity(activity.id); // IProps (without MobX)
-                  //   //setEditMode(false); // IProps (without MobX)
-
-                  // }}
-                  floated="right"
-                  content="View"
-                  color="blue"
-                ></Button>
-                <Button
-                  name={activity.id}
-
-                  //event "e" will be used for passing button properties in this case the "name" to the deleteActivity method. Event e is a type of SyentheticEvent
-                  onClick={(e) => {
-                   // deleteActivity(e, activity.id);
-                  }}
-                  //loading={deleteActivityId === activity.id &&submitState}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                >  
-                </Button>
                 <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
-    )
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Icon name="clock" /> {activity.date}
+        <Icon name="marker" /> {activity.city}
+      </Segment>
+      <Segment secondary>Attendees will go here</Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        {/** currentSelectedActivity is the handleSelectedActivity handler passed/originated in App.tsx */}
+        {/** setEditMode is the handleSelectedActivity handler passed/originated in App.tsx */}
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`} // Routes integration
+          // onClick={() => {
+          //   setSelectActivity(activity.id) //After MobX integration
+          //   //currentSelectedActivity(activity.id); // IProps (without MobX)
+          //   //setEditMode(false); // IProps (without MobX)
+
+          // }}
+          floated="right"
+          content="View"
+          color="blue"
+        ></Button>
+        <Button
+          name={activity.id}
+          //event "e" will be used for passing button properties in this case the "name" to the deleteActivity method. Event e is a type of SyentheticEvent
+          onClick={e => {
+            // deleteActivity(e, activity.id);
+          }}
+          //loading={deleteActivityId === activity.id &&submitState}
+          floated="right"
+          content="Delete"
+          color="red"
+        ></Button>
+      </Segment>
+    </Segment.Group>
+  );
 };
 
-export default observer(ActivityListItem)
+export default observer(ActivityListItem);
