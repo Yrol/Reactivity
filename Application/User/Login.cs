47607481c0d77792e66b8e256aeddbc11acc4ae0
unknown <yrollgayanth@yahoo.com>
2020-02-06 22:38:16 +1100

    Creating the Login handler

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Activities.Errors;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -31,14 +33,36 @@ namespace Application.User
         //returning AppUser
         public class Handler : IRequestHandler<Query, AppUser>
         {
-            public Handler(UserManager<AppUser> userManager, Microsoft.AspNetCore.Identity.SignInManager<AppUser> signInManager)
+            private readonly UserManager<AppUser> _userManager;
+            private readonly SignInManager<AppUser> _signInManager;
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
             {
-
+                _userManager = userManager;
+                _signInManager = signInManager;
             }
 
             //returning AppUser
             public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
             {
+                var user = await _userManager.FindByEmailAsync(request.Email);
+
+                //if the user is not found throw an exception
+                if (user == null){
+                    throw new RestExceptions(HttpStatusCode.Unauthorized);
+                }
+
+                //check password when signing in
+                //the 'false' will make sure the user will NOT be lockedout in case of failure attempt
+                var results = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                //if login is successful
+                if  (results.Succeeded)
+                {
+                    //TO do generate token
+                    return user;
+                }
+
+                throw new RestExceptions(HttpStatusCode.Unauthorized);
             }
         }
     }
