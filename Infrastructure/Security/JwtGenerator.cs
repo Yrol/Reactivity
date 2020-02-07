a58ccd515adcc54dd7e3c0ebeb70e204ecd5bb46
unknown <yrollgayanth@yahoo.com>
2020-02-07 22:54:47 +1100

    Adding the token generation mechanism

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Application.Interfaces;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
@@ -7,7 +13,31 @@ namespace Infrastructure.Security
     {
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            //data 
+            var claims = new List<Claim>
+            {
+                //this will add the username (get from "user.UserName") to the "nameid" property of the JWT token
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            //generate signing credentials that requires to sign the token from our API before it get sent to the user
+            //This information will be used to validate token (without  having to query the DB)
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Data inside the token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7), //expiry date (can be used for 7 days)
+                SigningCredentials = creds
+            };
+
+            //generating and writing the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token  = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
