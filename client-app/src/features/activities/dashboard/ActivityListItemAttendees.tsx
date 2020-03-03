70005261f93c45f74caf910b8ea0c7c2d1a54872
unknown <yrollgayanth@yahoo.com>
2020-03-03 22:37:41 +1100

    Adding the attendees list

--- client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, Image } from "semantic-ui-react";
+import { List, Image, Popup } from "semantic-ui-react";
 import { IAttendee } from "../../../models/activity";
 
 interface IProps {
@@ -13,7 +13,13 @@ export const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
       {attendees.map(attendee => (
         //Assigning a key to each item (otherwise will throw a unique key error)
         <List.Item key={attendee.username}>
-          <Image size="mini" circular src={attendee.image || "/assets/logo192.png"} />
+            {/* Pop will display the name when hover over the icon*/}
+            <Popup 
+                header={attendee.displayName}
+                trigger={
+                    <Image size="mini" circular src={attendee.image || "/assets/logo192.png"} />
+                }
+            />
         </List.Item>
       ))}
     </List>
